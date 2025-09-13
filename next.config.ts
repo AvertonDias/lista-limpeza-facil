c4267ba89d6547d9984514881bac62a54fd7f2f3
Averton Silva Dias <verton3@gmail.com>
2025-09-13 15:56:10 +0000

    main-app.js?v=1757778907186:2293 Download the React DevTools for a bette

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -35,4 +36,12 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+  },
+});

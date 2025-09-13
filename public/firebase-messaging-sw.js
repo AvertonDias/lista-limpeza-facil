bb1cbdb0f4b57ca74c917168842958ae735336a5
Averton Silva Dias <verton3@gmail.com>
2025-09-13 15:32:53 +0000

    main-app.js?v=1757777521535:2293 Download the React DevTools for a bette

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,9 +1,8 @@
-// Use importScripts para carregar o SDK do Firebase
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+// Este script precisa usar a sintaxe 'importScripts' pois é um Service Worker
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js");
 
-// Suas credenciais do Firebase
-// É seguro tê-las aqui, pois o Service Worker é um código do lado do cliente.
+// A configuração do Firebase é lida de um endpoint especial que o next-pwa cria
 const firebaseConfig = {
   apiKey: "AIzaSyDizs1-cOZnBX5ilBXazQIuFJD_sUnkDCQ",
   authDomain: "studio-1326322560-ad791.firebaseapp.com",
@@ -13,24 +12,20 @@ const firebaseConfig = {
   appId: "1:417616889091:web:f2c93816e5eaec7ff4d536"
 };
 
-// Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Obtém uma instância do Messaging.
 const messaging = firebase.messaging();
 
-// Adiciona um "ouvinte" para mensagens recebidas em segundo plano.
-messaging.onBackgroundMessage(function (payload) {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
   
+  // Extrai o título e as opções da notificação do payload
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.image, // Garante que o ícone seja usado
+    icon: '/images/placeholder-icon.png?v=2' // Caminho para o ícone
   };
 
+  // Exibe a notificação
   self.registration.showNotification(notificationTitle, notificationOptions);
 });

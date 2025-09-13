b122c22ccd49908f0838b775cbe528bcfc0e5e16
Averton Silva Dias <verton3@gmail.com>
2025-09-13 00:53:49 +0000

    main-app.js?v=1757724791209:2293 Download the React DevTools for a bette

--- public/worker-firebase-messaging.js
+++ public/worker-firebase-messaging.js
@@ -1,13 +1,14 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDizs1-cOZnBX5ilBXazQIuFJD_sUnkDCQ",
-    authDomain: "studio-1326322560-ad791.firebaseapp.com",
-    projectId: "studio-1326322560-ad791",
-    storageBucket: "studio-1326322560-ad791.appspot.com",
-    messagingSenderId: "417616889091",
-    appId: "1:417616889091:web:f2c93816e5eaec7ff4d536"
+  apiKey: "AIzaSyDizs1-cOZnBX5ilBXazQIuFJD_sUnkDCQ",
+  authDomain: "studio-1326322560-ad791.firebaseapp.com",
+  projectId: "studio-1326322560-ad791",
+  storageBucket: "studio-1326322560-ad791.appspot.com",
+  messagingSenderId: "417616889091",
+  appId: "1:417616889091:web:f2c93816e5eaec7ff4d536"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -15,25 +16,20 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log('Mensagem recebida em segundo plano:', payload);
+  console.log('Mensagem recebida em segundo plano: ', payload);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon || '/images/placeholder-icon.png',
-    data: {
-        url: payload.fcmOptions?.link || 'https://lista-limpeza-facil.web.app/'
-    }
+    icon: payload.notification.icon || '/images/placeholder-icon.png?v=2',
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
-
   event.waitUntil(
-    clients.openWindow(event.notification.data.url)
+    clients.openWindow('https://lista-limpeza-facil.web.app/')
   );
 });

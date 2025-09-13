7c2f0c4a7f2f7315813ffc036af8f736a5ea915d
Averton Silva Dias <verton3@gmail.com>
2025-09-13 15:31:52 +0000

    main-app.js?v=1757777476644:2293 Download the React DevTools for a bette

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,9 @@
-// Import the Firebase app and messaging services
-import { initializeApp } from "firebase/app";
-import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+// Use importScripts para carregar o SDK do Firebase
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
-// Your web app's Firebase configuration
+// Suas credenciais do Firebase
+// É seguro tê-las aqui, pois o Service Worker é um código do lado do cliente.
 const firebaseConfig = {
   apiKey: "AIzaSyDizs1-cOZnBX5ilBXazQIuFJD_sUnkDCQ",
   authDomain: "studio-1326322560-ad791.firebaseapp.com",
@@ -12,17 +13,23 @@ const firebaseConfig = {
   appId: "1:417616889091:web:f2c93816e5eaec7ff4d536"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
 
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+// Obtém uma instância do Messaging.
+const messaging = firebase.messaging();
+
+// Adiciona um "ouvinte" para mensagens recebidas em segundo plano.
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
   
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.image, // Use the image from the payload
+    icon: payload.notification.image, // Garante que o ícone seja usado
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);

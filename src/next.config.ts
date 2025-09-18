a12d00f60f7bbaf46badfa5b33dd940cd6ecf914
Averton Silva Dias <verton3@gmail.com>
2025-09-18 01:24:10 +0000

    main-app.js?v=1758158610302:2293 Download the React DevTools for a bette

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from 'next';
+import withPWA from 'next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -35,4 +36,13 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+
+const pwaConfig = {
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  sw: 'firebase-messaging-sw.js',
+}
+
+export default withPWA(pwaConfig)(nextConfig);

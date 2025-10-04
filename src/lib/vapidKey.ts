a46e2ff5659810413e3ce6f16788114531a6e353
Averton Silva Dias <verton3@gmail.com>
2025-10-04 01:00:28 +0000

    .

--- src/lib/vapidKey.ts
+++ src/lib/vapidKey.ts
@@ -3,4 +3,4 @@
 // It's safe to expose this key on the client side.
 // This key is used to ensure that notifications sent to your application
 // are actually from your server.
-export const VAPID_KEY = "9pDmN7KZkv-CP3GJoEADGJR61BXA-yOArQLvmyp_UFE";
+export const VAPID_KEY = "BGgAa0C-KPGKCA0baZggbWM9qWrGyNw4JGyECyAVGoB3Y8F7KDbMEvX0JVh3EyqOj5SkS0ozHLrmWmzr8CsJvO8";

1c8ae2d4662e9e5049831ddfb5f242c6db1b32df
Averton Silva Dias <verton3@gmail.com>
2025-10-04 00:53:01 +0000

    Try fixing this error: `Console Error: FirebaseError: Messaging: A probl

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,30 @@
-/* eslint-disable no-undef */
+// Scripts for firebase and firebase messaging
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
+// Initialize the Firebase app in the service worker with the project config
 const firebaseConfig = {
   apiKey: "AIzaSyDizs1-cOZnBX5ilBXazQIuFJD_sUnkDCQ",
   authDomain: "studio-1326322560-ad791.firebaseapp.com",
   projectId: "studio-1326322560-ad791",
   storageBucket: "studio-1326322560-ad791.appspot.com",
   messagingSenderId: "417616889091",
-  appId: "1:417616889091:web:f2c93816e5eaec7ff4d536",
+  appId: "1:417616889091:web:f2c93816e5eaec7ff4d536"
 };
 
 firebase.initializeApp(firebaseConfig);
 
+// Retrieve an instance of Firebase Messaging so that it can handle background messages
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Mensagem recebida em background: ", payload);
+messaging.onBackgroundMessage(function(payload) {
+  console.log("Received background message ", payload);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.image || "/images/placeholder-icon.png?v=2",
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });

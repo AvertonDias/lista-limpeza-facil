d9c1752a6f137827a927ac3ae89e385aeec9c1f6
Averton Silva Dias <verton3@gmail.com>
2025-10-04 00:46:46 +0000

    Vamos organizar tudo de forma **passo a passo** para que seu PWA receba

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: "studio-1326322560-ad791",
   storageBucket: "studio-1326322560-ad791.appspot.com",
   messagingSenderId: "417616889091",
-  appId: "1:417616889091:web:f2c93816e5eaec7ff4d536",
+  appId: "1:417616889091:web:f2c93816e5eaec7ff4d536"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -17,7 +17,6 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
   console.log("Mensagem recebida em background: ", payload);
-  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,

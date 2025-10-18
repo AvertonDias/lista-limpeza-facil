a03eb53d4b02d0e12abd4d25798fd06f6fbc9517
Averton Silva Dias <verton3@gmail.com>
2025-10-18 01:25:59 +0000

    ele ainda esta fixo, não está movendo junto com o teclado

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -22,7 +22,7 @@ const DialogOverlay = React.forwardRef<
     ref={ref}
     className={cn(
       "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      "flex items-center justify-center p-4",
+      "flex items-start justify-center p-4 pt-16",
       className
     )}
     {...props}
